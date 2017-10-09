17fb53d24958b53f1a43639354ce9b4591d274d6
annabush092 <annabush092@gmail.com>
2017-10-09 13:23:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file
+function findMatching(driverArray, namestr) {
+  return driverArray.filter(
+    function(name){
+      return name.toLowerCase() === namestr.toLowerCase();
+    }
+  )
+}
+
+function fuzzyMatch(driverArray, beginwith) {
+  return driverArray.filter(
+    function(name){
+      return name.startsWith(beginwith);
+    }
+  )
+}
+
+function matchName(driverObjectArray, str) {
+  return driverObjectArray.filter(
+    function(obj){
+        return obj['name'] === str;
+    }
+  )
+}
